848412bb0d3e42fa00a039ebcda7c7c67a5d294e
Carl <94771147+BeetJuicer@users.noreply.github.com>
2023-03-31 23:01:58 +0800

    player controls + movement
    - imported the new unity input system
    - changed the player settings to use the new input system
    - player movement script

---
+++ UntitledPirateProject2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // InputActions
+    [SerializeField]
+    private InputActionReference movement;
+
+    // Input values
+    private Vector2 movementInput;
+
+    // Components
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
+    // Movement values
+    [SerializeField]
+    private float moveSpeed = 15f;
+    [SerializeField]
+    private float collisionOffset = 0.2f;
+    [SerializeField]
+    private ContactFilter2D movementFilter;
+
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        movementInput = movement.action.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        // Do nothing if idle.
+        if (movementInput == Vector2.zero) 
+        {
+            print("idle.");
+            return;
+        }
+        // Allow for one directional movement if trying to move diagonally and there is something blocking one direction
+        // ideally only return a bool, no side effect
+
+        bool success = TryMove(movementInput);
+
+        if (!success)
+        {
+            // try to move in the x direction
+            success = TryMove(new Vector2(movementInput.x, 0));
+        }
+
+        if (!success)
+        {
+            // try to move in the y direction
+            success = TryMove(new Vector2(0, movementInput.y));
+        }
+
+        // Check for flipping
+        if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction == Vector2.zero)
+        {
+            return false;
+        }
+
+        // Check for potential collisions along the direction of the player's movement at a distance of the collisionOffset
+        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.deltaTime + collisionOffset);
+
+        // Move if there are no potential collisions
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+
+        return false;
+    }
+}
