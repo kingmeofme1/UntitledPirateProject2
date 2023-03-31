ab5ec30474da4501cc2b06670a5ce99c8fb13f37
kingmeofme1 <kingmeofme@gmail.com>
2023-03-31 16:36:59 -0400

    added additional hole functionality
    the holes can be filled and now move!

--- UntitledPirateProject2/Assets/Scripts/HoleManager.cs
+++ UntitledPirateProject2/Assets/Scripts/HoleManager.cs
@@ -5,10 +5,56 @@ using TMPro;
 
 public class HoleManager : MonoBehaviour
 {
-    public void IsFixed() //Just disables the hole gameobject
+    [SerializeField] private Sprite holeLeaking;
+    [SerializeField] private Sprite holeFixed;
+
+    public List<GameObject> holeList;
+    public int activeHole = 0;
+    public ScoreManager scoreManager;
+
+    private void Start()
     {
-        gameObject.SetActive(false);
+        if(holeList != null && holeList.Count > 0)
+        {
+            activeHole = Random.Range(0, holeList.Count);
+            ActivateHole();
+        }
+        else
+        {
+            Debug.Log("Initialization error, need more holes");
+        }
     }
 
-    // TODO : make it so that holes re enable randomly after a hole is fixed
+    public void IsFixed(int holeID) //fixes hole, gets a new hole!
+    {
+        if(holeID == activeHole && holeList[holeID].TryGetComponent(out SpriteRenderer spriteRenderer))
+        {
+            spriteRenderer.sprite = holeFixed;
+            scoreManager.UpdateScore();
+            int nextHole = Random.Range(0, holeList.Count - 1); //picks a random hole, -1 for the case of being a dupe.
+            if(nextHole == activeHole) //if it is a dupe, since we offset by 1 above, we can increment to get a distinct hole.
+            {
+                activeHole++;
+            }
+            else //if the hole was already different, we can just use it then!
+            {
+                activeHole = nextHole;
+            }
+            ActivateHole();
+        }
+        else
+        {
+            Debug.Log("wrong hole!");
+        }
+    }
+
+    public void ActivateHole()
+    {
+        if(holeList[activeHole].TryGetComponent(out SpriteRenderer spriteRenderer))
+        {
+            spriteRenderer.sprite = holeLeaking;
+        }
+    }
+
+    
 }
