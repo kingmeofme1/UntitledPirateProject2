eb59eb090ab9309af602b3d3eb1e7b870a6debe1
kingmeofme1 <kingmeofme@gmail.com>
2023-04-01 00:56:51 -0400

    added goon and captain rotations

--- UntitledPirateProject2/Assets/Scripts/FollowWaypoints.cs
+++ UntitledPirateProject2/Assets/Scripts/FollowWaypoints.cs
@@ -16,12 +16,14 @@ public class FollowWaypoints : MonoBehaviour
     private int currentWaypointIndex = 0;
 
     private bool isStopped;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
 
     void Update()
     {
         Vector2 direction = waypoints[currentWaypointIndex] - (Vector2)transform.position;
         direction.Normalize();
-
+        RotateSprite(direction);
         // move towards the current waypoint
         if(!isStopped)
             transform.position += (Vector3)(direction * moveSpeed * Time.deltaTime);
@@ -64,5 +66,51 @@ public class FollowWaypoints : MonoBehaviour
     public float GetMoveSpeed()
     {
         return moveSpeed;
+    }
+
+    private void RotateSprite(Vector2 direction)
+    {
+        float unitAngle = CalculateAngle(direction);
+        if(unitAngle < 0)
+        {
+            unitAngle += 360;
+        }
+        if (unitAngle < 45 || unitAngle >= 315)
+        {
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.flipX = false;
+        }
+        else if (unitAngle >= 225)
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
+        else if (unitAngle >= 135)
+        {
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprites[2];
+        }
+    }
+
+    public float CalculateAngle(Vector2 vector)
+    {
+        float aimAngle;
+        if (vector.x == 0)
+        {
+            aimAngle = 90;
+        }
+        else
+        {
+            aimAngle = Mathf.Rad2Deg * Mathf.Atan(vector.y / vector.x);
+        }
+
+        if (vector.x < 0 || (vector.x == 0 && vector.y < 0))
+        {
+            aimAngle += 180;
+        }
+        return aimAngle;
     }
 }
--- UntitledPirateProject2/Assets/Scripts/PirateGoon.cs
+++ UntitledPirateProject2/Assets/Scripts/PirateGoon.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PirateGoon : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
     public RuleManager ruleManager;
     public PlayerMovement player;
 
@@ -23,12 +25,59 @@ public class PirateGoon : MonoBehaviour
             waypointScript.enabled = false;
             Vector2 direction = player.transform.position - transform.position;
             transform.position += (Vector3)(direction.normalized * waypointScript.GetMoveSpeed() * Time.deltaTime);
+            RotateSprite(direction);
             //chase player
         }
         else
         {
             waypointScript.enabled = true; //this puts them back on patrol
         }
+        
+    }
 
+    private void RotateSprite(Vector2 direction)
+    {
+        float unitAngle = CalculateAngle(direction);
+        if (unitAngle < 0)
+        {
+            unitAngle += 360;
+        }
+        if (unitAngle < 45 || unitAngle >= 315)
+        {
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.flipX = false;
+        }
+        else if (unitAngle >= 225)
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
+        else if (unitAngle >= 135)
+        {
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprites[2];
+        }
+    }
+
+    public float CalculateAngle(Vector2 vector)
+    {
+        float aimAngle;
+        if (vector.x == 0)
+        {
+            aimAngle = 90;
+        }
+        else
+        {
+            aimAngle = Mathf.Rad2Deg * Mathf.Atan(vector.y / vector.x);
+        }
+
+        if (vector.x < 0 || (vector.x == 0 && vector.y < 0))
+        {
+            aimAngle += 180;
+        }
+        return aimAngle;
     }
 }
